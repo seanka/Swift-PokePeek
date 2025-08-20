af5460789d8182e91e9ed07c270221796f15da76
seanka <and.seanka@gmail.com>
2025-08-20 14:46:03 +0700

    Initial Commit

---
+++ PokePeek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokePeek
+//
+//  Created by Sean Anderson on 20/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokePeek/PokePeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokePeekApp.swift
+//  PokePeek
+//
+//  Created by Sean Anderson on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PokePeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
