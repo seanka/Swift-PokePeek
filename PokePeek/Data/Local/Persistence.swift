24db4a340d359bae6d5fcf27b96677d78606f6e3
Sean Anderson <seankristian16@gmail.com>
2025-08-21 08:54:22 +0700

    Root :: Core Data - Init core data model - Add persistence helper class

---
+++ PokePeek/Data/Local/Persistence.swift
@@ -0,0 +1,27 @@
+//
+//  Persistence.swift
+//  PokePeek
+//
+//  Created by Sean Anderson on 20/08/25.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "PokePeekModel")
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
