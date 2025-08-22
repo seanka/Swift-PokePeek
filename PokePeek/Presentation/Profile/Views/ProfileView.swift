42af60c79c5ceb5b3152e6edd226eca9fc64b449
Sean Anderson <seankristian16@gmail.com>
2025-08-22 13:45:36 +0700

    Profile :: Refinement Design - Design refinement on ProfileView

--- PokePeek/Presentation/Profile/Views/ProfileView.swift
+++ PokePeek/Presentation/Profile/Views/ProfileView.swift
@@ -12,12 +12,24 @@ struct ProfileView: View {
     @StateObject var viewModel: ProfileViewModel
     
     var body: some View {
-        VStack() {
-            Text("Profile")
+        VStack(alignment: .leading, spacing: 10) {
             
-            Text(viewModel.userData?.name ?? "")
+            // Introduction
+            VStack(alignment: .leading) {
+                Text("Hey PokèPal,")
+                    .font(.system(size: 28, weight: .bold, design: .rounded))
+                    .foregroundColor(.primary)
+                
+                Text(viewModel.userData?.name ?? "")
+                    .font(.system(size: 32, weight: .bold, design: .rounded))
+                    .foregroundColor(Color(UIColor(red: 0.83, green: 0.70, blue: 0.25, alpha: 1.0)))
+            }
+            
             Text(viewModel.userData?.email ?? "")
-            Text("\(String(describing: viewModel.userData?.createdAt))")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            Spacer()
             
             Button("Logout") {
                 viewModel.logoutUser()
@@ -34,9 +46,12 @@ struct ProfileView: View {
                 }
             }).disposed(by: viewModel.disposeBag)
         }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 14)
     }
 }
 
 #Preview {
     ProfileView(viewModel: DependencyContainer.shared.provideProfileViewModel())
+        .environmentObject(AppRouter(userHelper: UserDataHelper(context: PersistenceController.shared.container.viewContext)))
 }
